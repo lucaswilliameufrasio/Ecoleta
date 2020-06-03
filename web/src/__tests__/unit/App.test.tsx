f32cfbdc1b0b5d98ae5827be1604e8f287fabf6e
Lucas William <lucaswilliameufrasio@gmail.com>
2020-06-03 04:26:49 -0300

    other: update test description

--- web/src/__tests__/unit/App.test.tsx
+++ web/src/__tests__/unit/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../../App';
 
-test('renders learn react link', () => {
+test('renders hello next level heading tag', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/hello next level week/i);
     expect(linkElement).toBeInTheDocument();
