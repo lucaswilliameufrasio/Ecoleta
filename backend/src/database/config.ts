5ea1c2c6ec0bfacb90e7eb3ee9e02bb93338b930
Lucas William <lucaswilliameufrasio@gmail.com>
2020-06-04 04:57:49 -0300

    internal:  use env variables on knex config

--- backend/src/database/config.ts
+++ backend/src/database/config.ts
@@ -1,6 +1,5 @@
 import path from 'path';
-// import Knex from 'knex';
-
+console.log(process.env.APP_URL);
 export const config = {
     development: {
         client: 'sqlite3',
@@ -31,11 +30,13 @@ export const config = {
     },
 
     staging: {
-        client: 'postgresql',
+        client: process.env.DB_CLIENT ?? 'pg',
         connection: {
-            database: 'ecoleta',
-            user: 'username',
-            password: 'password',
+            host: process.env.DB_HOST ?? '127.0.0.1',
+            port: process.env.DB_PORT ?? '5432',
+            database: process.env.DB_DATABASE ?? '',
+            user: process.env.DB_USERNAME ?? 'postgres',
+            password: process.env.DB_PASSWORD ?? '',
         },
         pool: {
             min: 2,
@@ -43,15 +44,18 @@ export const config = {
         },
         migrations: {
             tableName: 'knex_migrations',
+            directory: path.resolve(__dirname, 'migrations'),
         },
     },
 
     production: {
-        client: 'postgresql',
+        client: process.env.DB_CLIENT ?? 'pg',
         connection: {
-            database: 'ecoleta',
-            user: 'username',
-            password: 'password',
+            host: process.env.DB_HOST ?? '127.0.0.1',
+            port: process.env.DB_PORT ?? '5432',
+            database: process.env.DB_DATABASE ?? '',
+            user: process.env.DB_USERNAME ?? 'postgres',
+            password: process.env.DB_PASSWORD ?? '',
         },
         pool: {
             min: 2,
@@ -62,7 +66,3 @@ export const config = {
         },
     },
 };
-
-// const instance: Knex = Knex(configuration as Knex.Config);
-
-// export const knexCurrentDatabase = (): Knex => instance;
