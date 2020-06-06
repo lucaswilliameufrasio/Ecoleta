c7a4cf561985f37056707afe3f1a1b9c2b1db0fa
Lucas William <lucaswilliameufrasio@gmail.com>
2020-06-06 05:35:55 -0300

    refactor: add HOST env variable to image_url

--- backend/src/controllers/ItemsController.ts
+++ backend/src/controllers/ItemsController.ts
@@ -3,13 +3,14 @@ import knex from '../database/connection';
 
 class ItemsController {
     async index(request: Request, response: Response): Promise<Response> {
+        const host = process.env.HOST || '127.0.0.1';
         const items = await knex('items').select('*');
 
         const serializedItems = items.map((item) => {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`,
+                image_url: `http://${host}:3333/uploads/${item.image}`,
             };
         });
 
