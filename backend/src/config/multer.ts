0c37b8517a345a3061c95f8cf6d3fd9906739308
Lucas William <lucaswilliameufrasio@gmail.com>
2020-06-06 23:36:04 -0300

    feat: now multer only allow image selected types

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -2,9 +2,41 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+interface IMimeTypeMap {
+    [key: string]: string;
+}
+
+const MIME_TYPE_MAP: IMimeTypeMap = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg',
+};
+
 export default {
     storage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'uploads'),
+        destination(req, file, callback) {
+            const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+            let error = null;
+
+            const isValid = MIME_TYPE_MAP[file.mimetype];
+
+            if (!isValid) {
+                error = new Error('São aceitos apenas arquivos de imagem.');
+
+                callback(error, uploadFolder);
+
+                return req.res?.json({
+                    error: 'Bad request',
+                    message: '"image" field only accept jpg, jpeg or png',
+                    validation: {
+                        source: 'file',
+                        keys: ['image'],
+                    },
+                });
+            } else {
+                callback(null, uploadFolder);
+            }
+        },
         filename(request, file, callback) {
             const hash = crypto.randomBytes(6).toString('hex');
             const originalFileName = file.originalname.replace(/\s/g, '');
