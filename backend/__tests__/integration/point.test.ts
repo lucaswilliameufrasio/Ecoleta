30ba5c69abe491254fde46637f69608c73123e30
Lucas William <lucaswilliameufrasio@gmail.com>
2020-07-29 02:04:34 -0300

    refactor: use mockPoints instead of a hard-coded point

--- backend/__tests__/integration/point.test.ts
+++ backend/__tests__/integration/point.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import app from '../../src/app';
 import connection from '../../src/database/connection';
 import { config } from '../../src/database/config';
+import { mockPoints } from '../mocks';
 import path from 'path';
 
 describe('Point', () => {
@@ -23,61 +24,43 @@ describe('Point', () => {
 
     const imagePath = path.resolve(__dirname, '..', 'assets', 'roman-kraft-unsplash.jpg');
 
-    const denoPointInfo = {
-        name: 'Deno Supermarket',
-        email: 'contact@deno.com.br',
-        whatsapp: '+5562999999799',
-        latitude: -46.81273213,
-        longitude: -35.39238112,
-        city: 'Natal',
-        uf: 'RN',
-        items: '1,2,6',
-    };
-
-    const alouPointInfo = {
-        name: 'Alou Supermarket',
-        email: 'contact@alou.com.br',
-        whatsapp: '+5562999999999',
-        latitude: -46.81273213,
-        longitude: -35.39238112,
-        city: 'Altamira',
-        uf: 'PR',
-        items: '1,2,6',
-    };
-
     it('should be able to list all registered points', async () => {
         await connection.seed.run();
-        await request(app).post('/points').field(alouPointInfo).attach('image', imagePath);
 
-        await request(app).post('/points').field(denoPointInfo).attach('image', imagePath);
+        const point = mockPoints;
 
-        const cityParam = 'Natal';
-        const ufParam = 'RN';
-        const itemsParam = '1,2';
+        await request(app).post('/points').field(point).attach('image', imagePath);
+
+        const cityParam = encodeURI(point.city);
+        const ufParam = point.uf;
+        const itemsParam = point.items;
 
         const response = await request(app).get(`/points?city=${cityParam}&uf=${ufParam}&items=${itemsParam}`);
 
-        expect(response.body[0]).toHaveProperty('id', 2);
-        expect(response.body[0]).toHaveProperty('name', 'Deno Supermarket');
-        expect(response.body[0]).toHaveProperty('email', 'contact@deno.com.br');
-        expect(response.body[0]).toHaveProperty('uf', 'RN');
-        expect(response.body).toHaveLength(1);
+        expect(response.body[0]).toHaveProperty('name', point.name);
+        expect(response.body[0]).toHaveProperty('whatsapp', point.whatsapp);
+        expect(response.body[0]).toHaveProperty('uf', point.uf);
     });
 
     it('should be able to register a new point', async () => {
         await connection.seed.run();
 
-        const response = await request(app).post('/points').field(denoPointInfo).attach('image', imagePath);
+        const point = mockPoints;
+
+        const response = await request(app).post('/points').field(point).attach('image', imagePath);
 
         expect(response.body).toHaveProperty('id', 1);
-        expect(response.body).toHaveProperty('name', 'Deno Supermarket');
-        expect(response.body).toHaveProperty('whatsapp', '+5562999999799');
-        expect(response.body).toHaveProperty('uf', 'RN');
+        expect(response.body).toHaveProperty('name', point.name);
+        expect(response.body).toHaveProperty('whatsapp', point.whatsapp);
+        expect(response.body).toHaveProperty('uf', point.uf);
     });
 
     it('should be able to return only a specific point', async () => {
         await connection.seed.run();
-        await request(app).post('/points').field(denoPointInfo).attach('image', imagePath);
+
+        const point = mockPoints;
+
+        await request(app).post('/points').field(point).attach('image', imagePath);
 
         const response = await request(app).get(`/points/1`);
 
@@ -85,10 +68,10 @@ describe('Point', () => {
         const expectedImageName = `cb7bfa9e7fae-roman-kraft-unsplash.jpg`;
 
         expect(response.body.point).toHaveProperty('id', 1);
-        expect(response.body.point).toHaveProperty('name', 'Deno Supermarket');
+        expect(response.body.point).toHaveProperty('name', point.name);
         expect(response.body.point).not.toHaveProperty('image', expectedImageName);
         expect(response.body.point).not.toHaveProperty('image_url', expectedImageURL);
-        expect(response.body.point).toHaveProperty('uf', 'RN');
+        expect(response.body.point).toHaveProperty('uf', point.uf);
         expect(response.body.items[0]).toHaveProperty('title', 'Lâmpadas');
         expect(response.body.items).toHaveLength(3);
     });
